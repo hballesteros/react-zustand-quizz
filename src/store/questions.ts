bbda866e90ebfdec8689b0b1be753255d839b9fd
Hugo <hugo@boahstudio.com>
2023-05-02 11:17:18 -0300

    reset game

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -10,6 +10,7 @@ interface State {
   selectAnswer: (questionId: number, answerIndex: number) => void
   goNextQuestion: () => void
   goPreviousQuestion: () => void
+  reset: () => void
 }
 
 export const useQuestionsStore = create<State>()(persist((set, get) => {
@@ -79,8 +80,11 @@ export const useQuestionsStore = create<State>()(persist((set, get) => {
       if (previousQuestion >= 0) {
         set({ currentQuestion: previousQuestion })
       }
-    }
+    },
 
+    reset: () => {
+      set({ currentQuestion: 0, questions: [] })
+    }
   }
 }, {
   name: 'question'
